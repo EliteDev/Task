2731cc035cbfb25a196d8d826682d76464667182
DevAG <programador.andresgarcia@gmail.com>
2023-04-26 12:05:23 -0700

    Visual style correction and initial setup of JavaScript file

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("formulario");
+const inputTask = document.getElementById("task");
+const boton = document.getElementById("miBoton");
+const listTask = document.getElementById("list");

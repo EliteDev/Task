a208de2cfb1aa7191265a960b912ade9a7278c14
DevAG <programador.andresgarcia@gmail.com>
2023-05-01 12:14:46 -0700

    Added dynamic task adding and removal functionality with JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-const form = document.getElementById("formulario");
-const inputTask = document.getElementById("task");
-const boton = document.getElementById("miBoton");
-const listTask = document.getElementById("list");
+// obtén el formulario y la lista de tareas pendientes
+const formulario = document.getElementById("formulario");
+const botonesEliminar = document.querySelectorAll(".fa-trash");
+const listaTareas = document.getElementById("list");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nuevaTarea = document.getElementById("task").value;
+
+
+  const nuevoElemento = document.createElement("li");
+  nuevoElemento.innerHTML = `
+    <i class="fa-regular fa-circle"></i>
+    <p>${nuevaTarea}</p>
+    <i class="fa-solid fa-trash"></i>
+  `;
+
+  listaTareas.appendChild(nuevoElemento);
+  document.getElementById("task").value = "";
+});
+
+listaTareas.addEventListener('click', (event) => {
+  if (event.target.classList.contains('fa-trash')) {
+    const taskItem = event.target.parentElement;
+    taskItem.remove();
+  }
+});

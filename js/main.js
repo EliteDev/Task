a80a4bcb6b1d5fcd031917eee32decb44dfa0a60
DevAG <programador.andresgarcia@gmail.com>
2023-05-15 17:34:11 -0700

    Implement remove in localStorage and only key

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ if(!localStorage.getItem('tasks')) {
 } else {
   tasks = JSON.parse(localStorage.getItem('tasks'));
   renderTasks(tasks);
-}
+};
 
 formulario.addEventListener("submit", function(event) {
   event.preventDefault();
@@ -26,6 +26,7 @@ formulario.addEventListener("submit", function(event) {
   }
 
   tasks.push(task);
+  localStorage.setItem(`task-${tasks.length - 1}`, JSON.stringify(task));
   localStorage.setItem('tasks', JSON.stringify(tasks));
   renderTasks(tasks);
 });
@@ -42,30 +43,38 @@ searchInput.addEventListener("input", function() {
 
 function renderTasks(tasks) {
   listaTareas.innerHTML = "";
-  tasks.forEach(function(task) {
-    const nuevoElemento = document.createElement("li");
-    const tittleTask = document.createElement('h3');
-    const tittleDescription = document.createElement('p');
-    const icon = document.createElement('i');
-    icon.classList = 'fa-solid fa-trash'
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key.startsWith('task-')) {
+      const task = JSON.parse(localStorage.getItem(key));
+      const nuevoElemento = document.createElement("li");
+      const tittleTask = document.createElement('h3');
+      const tittleDescription = document.createElement('p');
+      const icon = document.createElement('i');
+      icon.classList = 'fa-solid fa-trash'
 
-    tittleTask.innerText = task.tittle;
-    tittleDescription.innerText = task.description;
+      tittleTask.innerText = task.tittle;
+      tittleDescription.innerText = task.description;
 
-    nuevoElemento.append(tittleTask);
-    nuevoElemento.append(tittleDescription);
-    nuevoElemento.append(icon);
+      nuevoElemento.append(tittleTask);
+      nuevoElemento.append(tittleDescription);
+      nuevoElemento.append(icon);
 
-    listaTareas.append(nuevoElemento);
-
-  });
+      listaTareas.append(nuevoElemento);
+    }
+  }
 }
 
 listaTareas.addEventListener('click', (event) => {
   if (event.target.classList.contains('fa-trash')) {
     const taskItem = event.target.parentElement;
+    const taskIndex = Array.from(taskItem.parentElement.children).indexOf(taskItem);
+
+    localStorage.removeItem(`task-${taskIndex}`);
+    tasks.splice(taskIndex, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks(tasks);
+    
     taskItem.remove();
   }
 });
-
-
